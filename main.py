74b1457d10d809fdba68125e3dc1fd993c679d27
Johnny <kineticblack@hotmail.com>
2015-09-25 14:26:03 +1200

    Added command line arguments and folders for different searches

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-""" Checks instagram's website for existing user names.
+""" Checks a dictionary against instagram users looking for free user names based on 404 response.
+    If a 404 is returned the username is not necessarily free as they seem to have an extensive forbidden word list.
 """
 
 import requests
 import time
 import random
+import sys
+import os
 
 
 def url(username):
@@ -17,38 +20,51 @@ def load_dict():
     return definitions
 
 
-def record_response(word, status):
+def record_response(word, status, word_len):
+    """ Writes the word to different files depending on server response.
+    """
     status_codes = {404: 'available.txt', 200: 'unavailable.txt'}
-    filename = status_codes.get(status, 'error.txt')
 
-    with open('tried.txt', 'a') as file:
+    filename = os.path.join(str(word_len), status_codes.get(status, 'error.txt'))
+    catch_all = os.path.join(str(word_len), 'tried.txt')
+
+    with open(catch_all, 'a') as file:
         if status not in status_codes:
             file.write('{} - {}\n'.format(word, status))
         else:
-	    file.write('{}\n'.format(word))
+            file.write('{}\n'.format(word))
 
     with open(filename, 'a') as file:
         file.write('{}\n'.format(word))
 
 
-def main():
+def main(word_len=None):
     definitions = [x.strip() for x in load_dict() if x.strip().isalpha()]
-    three_letter_words = [x for x in definitions if len(x) == 5]
+    if word_len:
+        n_letter_words = [x for x in definitions if len(x) == word_len]
+        definitions = n_letter_words
 
-    print len(three_letter_words)
+    if not word_len:
+        word_len = 'all'
+    if not os.path.exists(str(word_len)):
+        os.makedirs(str(word_len))
 
-    for word in three_letter_words:
+    for word in definitions:
         user_page = url(word)
         headers = {'user-agent': 'instauser/0.0.1'}
         r = requests.get(user_page, headers=headers)
 
-	if r.status_code == 404:
-        	print word
+        if r.status_code == 404:
+            print(word)
 
-        record_response(word, r.status_code)
+        record_response(word, r.status_code, word_len)
 
         time.sleep((random.random() * 0.5) + 0.5)  # Rate Limit
 
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) > 1:
+        word_length = int(sys.argv[1])
+        main(word_length)
+    else:
+        main()
